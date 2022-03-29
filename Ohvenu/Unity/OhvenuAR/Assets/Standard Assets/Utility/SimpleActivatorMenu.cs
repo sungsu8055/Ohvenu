1911bb3b3a8fbed6a7f037eeb80796590726c91e
sungsu8055 <98731917+sungsu8055@users.noreply.github.com>
2022-03-29 21:40:25 +0900

    03.29

--- Ohvenu/Unity/OhvenuAR/Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ Ohvenu/Unity/OhvenuAR/Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 #pragma warning disable 618
 namespace UnityStandardAssets.Utility
@@ -8,7 +9,7 @@ namespace UnityStandardAssets.Utility
     {
         // An incredibly simple menu which, when given references
         // to gameobjects in the scene
-        public GUIText camSwitchButton;
+        // public GUIText camSwitchButton;
         public GameObject[] objects;
 
 
@@ -19,7 +20,7 @@ namespace UnityStandardAssets.Utility
         {
             // active object starts from first in array
             m_CurrentActiveObject = 0;
-            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+            //camSwitchButton.text = objects[m_CurrentActiveObject].name;
         }
 
 
@@ -33,7 +34,7 @@ namespace UnityStandardAssets.Utility
             }
 
             m_CurrentActiveObject = nextactiveobject;
-            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+            //camSwitchButton.text = objects[m_CurrentActiveObject].name;
         }
     }
 }
